9e3cbdda4a0029064534b0cb3ba016ff3a671404
Rizki Gusna <gusnafarid@gmail.com>
2024-02-28 00:29:51 +0700

    add utils for save user to local storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,12 +1,24 @@
+import { User } from '@/features/auth';
+
 const storage = {
   getToken: () => {
-    return JSON.parse(window.localStorage.getItem('token') as string);
+    return window.localStorage.getItem('token');
   },
   setToken: (token: string) => {
     window.localStorage.setItem('token', token);
   },
   clearToken: () => {
     window.localStorage.removeItem('token');
+  },
+  getUser: () => {
+    const user = window.localStorage.getItem('user');
+    return user ? JSON.parse(user) : undefined;
+  },
+  setUser: (user: User) => {
+    window.localStorage.setItem('user', JSON.stringify(user));
+  },
+  clearUser: () => {
+    window.localStorage.removeItem('user');
   },
 };
 
