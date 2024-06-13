e49f0d17b7b70a8bd472d37f966004f4a1d0e722
Rizki Gusna <gusnafarid@gmail.com>
2024-06-14 00:23:18 +0700

    add react query config types

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,4 +1,21 @@
-import { QueryClient, DefaultOptions } from '@tanstack/react-query';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { QueryClient, DefaultOptions, UseMutationOptions } from '@tanstack/react-query';
+
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> = Awaited<
+  ReturnType<FnType>
+>;
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+  ReturnType<T>,
+  'queryKey' | 'queryFn'
+>;
+
+export type MutationConfig<MutationFnType extends (...args: any) => Promise<any>> =
+  UseMutationOptions<
+    ApiFnReturnType<MutationFnType>,
+    Error,
+    Parameters<MutationFnType>[0]
+  >;
 
 const queryConfig: DefaultOptions = {
   queries: {
