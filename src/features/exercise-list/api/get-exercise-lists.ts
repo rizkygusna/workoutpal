f1fad6c2b12148d8ebc77742ebffcc1bd8b3e548
Rizki Gusna <gusnafarid@gmail.com>
2024-06-14 00:25:55 +0700

    add query hook to get exercise lists

---
+++ src/features/exercise-list/api/get-exercise-lists.ts
@@ -0,0 +1,36 @@
+import { axiosInstance } from '@/lib/axios';
+import { QueryConfig } from '@/lib/react-query';
+import { queryOptions, useQuery } from '@tanstack/react-query';
+
+export interface ExerciseList {
+  id: string;
+  name: string;
+  description: string;
+}
+
+interface GetExerciseListsParams {
+  userId: string;
+}
+
+const getExerciseLists = (params: GetExerciseListsParams): Promise<ExerciseList[]> => {
+  return axiosInstance.get('/exerciseLists', { params });
+};
+
+export const getExerciseListsQueryOptions = (params: GetExerciseListsParams) => {
+  return queryOptions({
+    queryKey: ['exerciseLists', params],
+    queryFn: () => getExerciseLists(params),
+  });
+};
+
+type UseExerciseListsOptions = {
+  params: GetExerciseListsParams;
+  queryConfig?: QueryConfig<typeof getExerciseListsQueryOptions>;
+};
+
+export const useGetExerciseLists = ({ params, queryConfig }: UseExerciseListsOptions) => {
+  return useQuery({
+    ...getExerciseListsQueryOptions(params),
+    ...queryConfig,
+  });
+};
