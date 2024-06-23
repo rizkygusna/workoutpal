1ddacf3e7cd93cf71f36da1d0a182cb46ec9e49b
Rizki Gusna <gusnafarid@gmail.com>
2024-06-23 22:36:00 +0700

    add custom hook for delete exercise list

---
+++ src/features/exercise-list/api/delete-exercise-list.ts
@@ -0,0 +1,29 @@
+import { axiosInstance } from '@/lib/axios';
+import { MutationConfig } from '@/lib/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const deleteExerciseList = (listId: string) => {
+  return axiosInstance.delete('/exerciseList/' + listId);
+};
+
+type UseDeleteExerciseListOptions = {
+  mutationConfig?: MutationConfig<typeof deleteExerciseList>;
+};
+
+export const useDeleteExerciseList = ({
+  mutationConfig,
+}: UseDeleteExerciseListOptions = {}) => {
+  const queryClient = useQueryClient();
+  const { onSuccess, ...restConfig } = mutationConfig || {};
+
+  return useMutation({
+    onSuccess: (...args) => {
+      queryClient.invalidateQueries({
+        queryKey: ['exerciseLists'],
+      });
+      onSuccess?.(...args);
+    },
+    ...restConfig,
+    mutationFn: deleteExerciseList,
+  });
+};
